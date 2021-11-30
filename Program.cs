395d97818f757683a19a2fd1c530bec00167c653
erlonbie <erlonminievix@gmail.com>
2021-11-30 16:26:07 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace dotnetcore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            if (true)
+            {
+                Console.WriteLine("teste123");
+            }
+        }
+    }
+}
